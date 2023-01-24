a21fb29c5a8138e879c2a9f470c4d8b03e6745bd
jluethi <joel.luethi@uzh.ch>
2023-01-24 16:22:17 +0100

    Add channel selection and multi-channel loading

--- src/napari_ome_zarr_roi_loader/roi_loader_widget.py
+++ src/napari_ome_zarr_roi_loader/roi_loader_widget.py
@@ -7,15 +7,10 @@ Joel Luethi, joel.luethi@fmi.ch
 import os
 
 import napari
-import zarr
 from magicgui.widgets import ComboBox, Container, FileEdit, PushButton
 from napari.utils.notifications import show_info
 
-from napari_ome_zarr_roi_loader.utils import (
-    get_metadata,
-    load_intensity_roi,
-    read_roi_table,
-)
+from napari_ome_zarr_roi_loader.utils import load_intensity_roi
 
 
 class RoiLoader(Container):
@@ -54,29 +49,21 @@ class RoiLoader(Container):
         )
 
     def run(self):
-        roi_table = self._roi_table_picker.value
-        roi_name = self._roi_picker.value
-        level = self._level_picker.value
-        channel = self._channel_picker.value
-
+        selected_value = self._roi_table_picker.value
         img_roi, scale_img = load_intensity_roi(
             zarr_url=self._zarr_url_picker.value,
-            roi_of_interest=roi_name,
-            channel_index=channel,
-            level=level,
-            roi_table=roi_table,
+            roi_of_interest=self._roi_picker.value,
+            channel_index=self._channel_picker.value,
+            level=self._level_picker.value,
+            roi_table=self._roi_table_picker.value,
         )
         self._viewer.add_image(img_roi, scale=scale_img)
 
         # FIXME: For some reason, running currently resets the
         # self._roi_table_picker choices to an empty list. This works around
-        # that. No idea why this reset is happening though. See
-        # https://github.com/jluethi/napari-ome-zarr-roi-loader/issues/3
+        # that. No idea why this reset is happening though.
         self.update_roi_tables()
-        self._roi_table_picker.value = roi_table
-        self._roi_picker.value = roi_name
-        self._level_picker.value = level
-        self._channel_picker.value = channel
+        self._roi_table_picker.value = selected_value
 
     def update_roi_tables(self):
         """
@@ -85,9 +72,8 @@ class RoiLoader(Container):
         self._roi_table_picker.choices = self._get_roi_table_choices()
 
     def update_roi_selection(self):
-        self._roi_picker.choices = self._get_roi_choices()
-        self._channel_picker.choices = self._get_channel_choices()
-        self._level_picker.choices = self._get_level_choices()
+        print("ROI table picker has changed")
+        # TODO: Trigger update of level, channels & ROI choices
 
     def _get_roi_table_choices(self):
         try:
@@ -112,30 +98,13 @@ class RoiLoader(Container):
             return [""]
 
     def _get_roi_choices(self):
-        if not self._roi_table_picker.value:
-            # When no roi table is provided.
-            # E.g. during bug with self._roi_table_picker reset
-            return [""]
-        try:
-            roi_table = read_roi_table(
-                self._zarr_url_picker.value, self._roi_table_picker.value
-            )
-            return list(roi_table.obs_names)
-        except zarr.errors.PathNotFoundError:
-            return [""]
+        # FIXME: Add actual inference for choices
+        return ["FOV_36"]
 
     def _get_channel_choices(self):
         # FIXME: Add actual inference for choices
         return [0]
 
     def _get_level_choices(self):
-        try:
-            metadata = get_metadata(self._zarr_url_picker.value)
-            dataset = 0  # FIXME, hard coded in case multiple multiscale
-            # datasets would be present & multiscales is a list
-            nb_levels = len(metadata.attrs["multiscales"][dataset]["datasets"])
-            return list(range(nb_levels))
-        except KeyError:
-            # This happens when no valid OME-Zarr file is selected, thus no
-            # metadata file is found & no levels can be set
-            return [""]
+        # FIXME: Add actual inference for choices
+        return [0, 1, 2, 3, 4]
--- src/napari_ome_zarr_roi_loader/utils.py
+++ src/napari_ome_zarr_roi_loader/utils.py
@@ -1,5 +1,3 @@
-from functools import lru_cache
-from pathlib import Path
 from typing import Iterable, List
 
 import anndata as ad
@@ -190,19 +188,6 @@ def convert_ROI_table_to_indices(
 #     return colors_simple, label_properties
 
 
-@lru_cache(maxsize=16)
-def get_metadata(zarr_url):
-    with zarr.open(zarr_url) as metadata:
-        return metadata
-
-
-@lru_cache(maxsize=16)
-def read_roi_table(zarr_url: Path, roi_table):
-    # FIXME: Make this work for cloud-based files => different paths
-    table_url = zarr_url / f"tables/{roi_table}"
-    return ad.read_zarr(table_url)
-
-
 def load_intensity_roi(
     zarr_url,
     roi_of_interest,
@@ -217,15 +202,15 @@ def load_intensity_roi(
     # image per well) => FIXME for multiplexing
 
     # Get the ROI table
-    roi_an = read_roi_table(zarr_url, roi_table)
+    roi_an = ad.read_zarr(zarr_url / f"tables/{roi_table}")
 
     # Load the pixel sizes from the OME-Zarr file
     dataset = 0  # FIXME, hard coded in case multiple multiscale
     # datasets would be present & multiscales is a list
-    metadata = get_metadata(zarr_url)
-    scale_img = metadata.attrs["multiscales"][dataset]["datasets"][level][
-        "coordinateTransformations"
-    ][0]["scale"]
+    with zarr.open(zarr_url) as metadata:
+        scale_img = metadata.attrs["multiscales"][dataset]["datasets"][level][
+            "coordinateTransformations"
+        ][0]["scale"]
 
     # Get ROI indices for labels
     indices_dict = convert_ROI_table_to_indices(
